e09b0f31f5b7c7282ce5ac5707408042dd098760
Rene Cortez <reco@mando.attlocal.net>
2022-02-16 19:57:10 -0600

    Added the wasmpackplugin Added crate type in toml file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HTMLWebpakPlugin = require('html-webpack-plugin')
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin')
+const { resolve } = require('path')
 
 module.exports = {
     entry: './public/main.js',
@@ -10,6 +12,9 @@ module.exports = {
     plugins: [
         new HTMLWebpakPlugin({
             template: './public/index.html'
+        }),
+        new WasmPackPlugin({
+            createDirectory: path/resolve(__dirname, '.')
         })
     ]
 }
